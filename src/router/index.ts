672acd0419b4223531c7045e43d02d0a1211a969
aiokr <hiaiokr@gmail.com>
2024-04-28 00:29:29 +0800

    feat: 开始添加单独照片页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MapView from '../views/MapView.vue'
+import ImageView from '../views/ImageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: 'map',
       component: MapView,
       props: { isMap: true }
+    },
+    {
+      path: '/image/:id',
+      name: 'image',
+      component: ImageView,
     },
     {
       path: '/about',
---
+++ src/views/ImageView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ImageViewer from '@/components/ImageViewer.vue';
+import { useViewModeStore } from '@/stores/viewMode';
+import { isMapView } from '@/stores/mapStore';
+</script>
+
+<template>
+<div></div>
+</template>
